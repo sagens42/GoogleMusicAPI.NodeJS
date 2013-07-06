02b7fcb94d5bff10dc22942533602a854d4cc984
Mathew Huusko V <mhuusko5@gmail.com>
2013-07-06 17:46:33 -0400

    Example actually runs now. Shows that login fails.

--- example/list-example.js
+++ example/list-example.js
@@ -1,15 +1,10 @@
-﻿var http = require("http");
-var GoogleMusic = require('googlemusicapi').GoogleMusicApi;
-http.createServer(onRequest).listen(8888);
-function onRequest(request, response) {
-	var googlemusic = new GoogleMusic('user@gmail.com', 'password');
-	googlemusic.Login(function () {
-		googlemusic.GetAllSongs('', function(result) {
-			var length = result.length;
-			var i;
-			for (i=0;i<length; i++) {
-				console.log(result[i].title);
-			}
-		}); 
-	});
-}
+var GoogleMusicApi = require('../index.js').GoogleMusicApi;
+
+var gMusic = new GoogleMusicApi('email', 'password');
+gMusic.Login(function() {
+    gMusic.GetAllSongs('', function(result) {
+        for (var i = 0; i < result.length; i++) {
+            console.log(result[i].title);
+        }
+    });
+});
